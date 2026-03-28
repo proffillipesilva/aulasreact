c91a3e3ad88e0d5cfa41eb1ea23d87b61cc60b90
JEFFERSON LUIZ FERRAZINI JUNIOR <35320@fiecalunos.lan>
2026-03-27 21:22:58 -0300

    ok

---
+++ src/Home/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo!</h1>
+      <p>Bem-vindo à página inicial!</p>
+    </div>
+  );
+};
+
+export default HomePage;
