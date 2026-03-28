27eaf54de9ca56c44783abb02ce60f4c3664a824
PEDRO HENRIQUE ESTRIOLI <46132@fiecalunos.lan>
2026-03-27 21:32:52 -0300

    27/03/26

---
+++ src/home/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const HomePage = props => {
+  return (
+    <div>
+        <h1>HomePage</h1>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/b/b8/Messi_vs_Nigeria_2018.jpg'/>
+        </div>
+  )
+}
+
+HomePage.propTypes = {}
+
+export default HomePage
